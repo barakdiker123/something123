380129955c648dce5d00df39e655c959be73b3d1
barak diker <barakdiker@gmail.com>
2023-04-30 00:12:07 +0300

    Added commit

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 
 
-
 #include <iostream>
-#include <opencv2/opencv.hpp> 
+#include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
 
-int main(){
-	std::cout << " Hello World " << std::endl;
-	cv::Mat image = cv::imread("starry_night.jpg");
-	cv::Mat mat;
-	cv::imshow("Hello World" , image);
-	cv::VideoCapture vc(0);
-	vc.read(mat);
-	cv::imshow("barak" ,mat);
-	cv::waitKey(0);
-	
+/**
+ * Barak The King
+ *
+ *
+ *
+ * */
+
+int main() {
+  std::cout << " Hello World " << std::endl;
+  cv::Mat image = cv::imread("starry_night.jpg");
+  cv::Mat mat;
+  cv::imshow("Hello World", image);
+  cv::VideoCapture vc(0);
+  vc.read(mat);
+  cv::imshow("barak", mat);
+  cv::waitKey(0);
 }
 
-//int function(int a , int b){
+// int function(int a , int b){
 //	std::cout << "barak " ;
-//}
+// }
