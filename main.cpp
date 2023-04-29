0ade7172f3f1e456b5b6e18a9f8c1419aa55c41c
barak diker <barakdiker@gmail.com>
2023-04-30 00:07:05 +0300

    First commit of the project

---
+++ main.cpp
@@ -0,0 +1,22 @@
+
+
+
+#include <iostream>
+#include <opencv2/opencv.hpp> 
+#include <opencv2/videoio.hpp>
+
+int main(){
+	std::cout << " Hello World " << std::endl;
+	cv::Mat image = cv::imread("starry_night.jpg");
+	cv::Mat mat;
+	cv::imshow("Hello World" , image);
+	cv::VideoCapture vc(0);
+	vc.read(mat);
+	cv::imshow("barak" ,mat);
+	cv::waitKey(0);
+	
+}
+
+//int function(int a , int b){
+//	std::cout << "barak " ;
+//}
